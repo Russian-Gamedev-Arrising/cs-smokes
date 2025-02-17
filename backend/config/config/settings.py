33f5d23c81b353c43292d70e1093741a5f68abb9
g0ldexperiencerequiem <pborisenko2006@yandex.ru>
2025-02-17 13:29:25 +0300

    Docker делаю рот дани ебал

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,24 +19,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-moj$-u9++dxmdr)mmpq5be&+$bs)l-p9bim^eta#4a5$^wc+=0"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0','*']
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'config', 
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +80,14 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': os.getenv('POSTGRES_DB', 'DB_NAME'),  
+        'USER': os.getenv('POSTGRES_USER', 'DB_USER'), 
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'DB_PASSWORD'),  
+        'HOST': os.getenv('DB_HOST', 'db'),  
+        'PORT': os.getenv('DB_PORT', '5432'),  
     }
 }
 
